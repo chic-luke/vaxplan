5db508281ace7a830b8013bf0d8745a69154a38e
Luca <72322463+chic-luke@users.noreply.github.com>
2021-06-29 16:33:47 +0200

    Minor refactoring

--- src/main/java/it/vaxplan/backend/Vaccine.java
+++ src/main/java/it/vaxplan/backend/Vaccine.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
