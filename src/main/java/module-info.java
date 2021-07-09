8a1f0c8405a480eb7cbbc354f3324099e3f48459
Luca <72322463+chic-luke@users.noreply.github.com>
2021-07-09 10:53:55 +0200

    Update dependencies (trying out jphoenix), delete unnecessary resources

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module it.vaxplan {
     requires javafx.fxml;
     requires static lombok;
     requires java.validation;
+    requires com.jfoenix;
 
     opens it.vaxplan to javafx.fxml;
     exports it.vaxplan.frontend;
