d1d4caf5073d663f05ab0e8d915c0b74051e77c8
Luca <72322463+chic-luke@users.noreply.github.com>
2021-07-14 16:08:47 +0200

    Fix previous commit

--- src/main/java/it/vaxplan/backend/App.java
+++ src/main/java/it/vaxplan/backend/App.java
@@ -9,5 +9,25 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+
+        Patient p1 = Patient.builder()
+                .firstName("Mario")
+                .lastName("Rossi")
+                .birthDay(DateHandler.parse("05/04/1986", null))
+                .sex(Sex.MALE)
+                .fiscalCode("RSSMRA86D05F205W")
+                .birthPlace("Milano")
+                .healthCareWorker(false)
+                .build();
+
+        Booking b1 = Booking.builder()
+                .patient(p1)
+                .vaccine(Vaccine.COVID)
+                .date(DateHandler.parse("01/09/2021", null))
+                .location("Milan")
+                .build();
+
+        System.out.println(b1.getDate());
     }
 }
--- src/main/java/it/vaxplan/backend/DateHandler.java
+++ src/main/java/it/vaxplan/backend/DateHandler.java
@@ -1,2 +1,30 @@
-package it.vaxplan.backend;public class DateHandler {
+package it.vaxplan.backend;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateHandler {
+
+    /**
+     * Parses a date string into a ZoneDateTime object
+     * @param date String formatted like "dd/mm/yyyy"
+     * @return Corresponding ZoneDateTime object
+     */
+    public static ZonedDateTime parse(String date, String time) {
+
+        var ld = LocalDate.parse(
+                date,
+                DateTimeFormatter.ofPattern("dd/MM/uuuu")
+        );
+
+        var lt = LocalTime.of(0, 0);
+        if (time != null)
+            lt = LocalTime.parse(time);
+
+        return ZonedDateTime.of(ld, lt, ZoneId.of("GMT+1"));
+    }
+
 }
