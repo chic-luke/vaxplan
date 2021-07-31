1ad0a2e59288c972ca24b8373d8fad02f845d2f0
Luca <72322463+chic-luke@users.noreply.github.com>
2021-07-31 21:35:13 +0200

    PatientService: Add addPatients and removePatients methods to add/remove a Collection of patients rather than individually

--- src/main/java/it/vaxplan/backend/service/PatientService.java
+++ src/main/java/it/vaxplan/backend/service/PatientService.java
@@ -3,6 +3,7 @@ package it.vaxplan.backend.service;
 import it.vaxplan.backend.Patient;
 import lombok.Getter;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -25,6 +26,22 @@ public class PatientService {
      */
     public void removePatient(Patient patient) {
         patients.remove(patient);
+    }
+
+    /**
+     * Add all the Patients in a Collection to the set of patients
+     * @param patientsToAdd Collection of Patients to add
+     */
+    public void addPatients(Collection<Patient> patientsToAdd) {
+        patients.addAll(patientsToAdd);
+    }
+
+    /**
+     * Remove all the Patients in a Collection from the set of patients
+     * @param patientsToAdd Collection of Patients to remove
+     */
+    public void removePatients(Collection<Patient> patientsToAdd) {
+        patients.removeAll(patientsToAdd);
     }
 
 }
